9f32c4374a8742c12ff24c3ef305e229b4a013b7
bhupesh003 <ygbhupesh003@gmail.com>
2025-08-10 18:11:48 +0530

    cors fixz

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,21 @@ import reportRoutes from './routes/report.routes';
 import { zodErrorHandler } from './middlewares/zodErrorHandler';
 import errorHandler from '@middlewares/errorHandler';
 import { notFoundHandler } from '@middlewares/notFoundHandler';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs as any));
-
+// Allow requests from your frontend origin only (replace with your actual frontend URL)
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true, // if you want to allow cookies/auth headers
+  }),
+);
 // API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/plants', plantRoutes);
