da6ff669734d768f21d325f4e4cada4f20c0d04b
bhupesh003 <ygbhupesh003@gmail.com>
2025-09-21 19:32:30 +0530

    ci/cd and prebuild hooks

---
+++ .platform/hooks/prebuild/01_install_chrome_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install -y atk at-spi2-atk cups-libs libXcomposite libXcursor \
+libXdamage libXext libXi libXrandr libXrender libXScrnSaver \
+libXtst pango gtk3 alsa-lib nss libdrm libgbm
